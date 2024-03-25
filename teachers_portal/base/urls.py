dfdc37f208e3219da7dedd679bc99ea4a2c76900
zEleceed <133176949+zEleceed@users.noreply.github.com>
2024-03-25 16:02:12 +0900

    urls.py/views.py updated

--- teachers_portal/base/urls.py
+++ teachers_portal/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 
 
 urlpatterns = [
+    path("post/new/", BlogCreateView.as_view(), name="classroom_new"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("", BlogListView.as_view(), name="home"),
 
--- teachers_portal/base/views.py
+++ teachers_portal/base/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 
 # Create your views here.
 
-class BlogListView(ListView):
+class BlogListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = "home.html"
 
 
-class BlogDetailView(DetailView):
+class BlogDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "classroom_new.html"
+    fields = ("title", "body")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
